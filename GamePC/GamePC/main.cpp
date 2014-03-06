52d28fe9261e18d9ea8ce8d2d3392a0dcf996b7d
ky525 <yusuke_ilk525@hotmail.com>
2014-03-07 02:03:35 +0900

    ウィンドウモード化
    テツ！

--- GamePC/GamePC/main.cpp
+++ GamePC/GamePC/main.cpp
@@ -13,11 +13,28 @@
 //-----------------------------------------------------
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nCmdShow )
 {
+	ChangeWindowMode(true);
+	SetGraphMode(1280, 720, 32);
 	if( DxLib_Init() == -1 )		// ＤＸライブラリ初期化処理
 	{
 		return -1 ;			// エラーが起きたら直ちに終了
 	}
-	DrawPixel( 320 , 240 , 0xffff ) ;	// 点を打つ
+
+	// テ
+	DrawLine(200,200,400,200,0xffffffff);
+	DrawLine(150,300,450,300,0xffffffff);
+	DrawLine(300,300,250,500,0xffffffff);
+
+	// ツ
+	DrawLine(500,200,550,300,0xffffffff);
+	DrawLine(600,200,650,300,0xffffffff);
+	DrawLine(800,200,650,500,0xffffffff);
+
+	// ツ
+	DrawLine(1000,200,1000,400,0xffffffff,15);
+	DrawLine(1000,485,1000,500,0xffffffff,15);
+
+
 	WaitKey() ;				// キー入力待ち
 	DxLib_End() ;				// ＤＸライブラリ使用の終了処理
 	return 0 ;				// ソフトの終了 
