5b10fa82deb6b279469cb0b4fff356f187d85129
ky525 <yusuke_ilk525@hotmail.com>
2014-03-09 23:11:03 +0900

    不要部分除去

--- GamePC/GamePC/main.cpp
+++ GamePC/GamePC/main.cpp
@@ -26,24 +26,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine,
 		return -1;
 	}
 
-#if 0
-	// テ
-	DrawLine(200,200,400,200,0xffffffff);
-	DrawLine(150,300,450,300,0xffffffff);
-	DrawLine(300,300,250,500,0xffffffff);
-
-	// ツ
-	DrawLine(500,200,550,300,0xffffffff);
-	DrawLine(600,200,650,300,0xffffffff);
-	DrawLine(800,200,650,500,0xffffffff);
-
-	// ！
-	DrawLine(1000,200,1000,400,0xffffffff,15);
-	DrawLine(1000,485,1000,500,0xffffffff,15);
-
-	WaitKey() ;				// キー入力待ち
-#endif
-
 	// 描画先を裏画面に設定
 	SetDrawScreen(DX_SCREEN_BACK);
 
